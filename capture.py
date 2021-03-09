4d33f672ab32bf3cafb18cf2984c7baf5cae0946
Thomas Rausch <thomas@thrau.at>
2021-03-09 19:07:31 +0100

    Fix error in VideoCapture property setup

--- capture.py
+++ capture.py
@@ -84,7 +84,7 @@ def main():
 
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     while True:
         more, frame = cap.read()
